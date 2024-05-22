e25940ee6d6a8a031ff0e120abb46ffe07ac8814
molly-culot <molly.culot@icloud.com>
2024-05-22 15:57:45 -0400

    q=works

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,35 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-// Filename - App.js
- 
-import React from "react";
-import Navbar from "./components/Navbar";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
-import Home from "./pages";
-import LogSchedule from "./pages/LogSchedule";
-import SignUp from "./pages/SignUp";
-import ViewSchedule from "./pages/ViewSchedule";
- 
+
+
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route
-                    path="/contact"
-                    element={<Contact />}
-                />
-                <Route path="/blogs" element={<Blogs />} />
-                <Route
-                    path="/sign-up"
-                    element={<SignUp />}
-                />
-            </Routes>
-        </Router>
-    );
+  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const handleUsernameChange = (event) => {
+  const cleanedValue = event.target.value.replace(/\s/g, '');
+    setUsername(cleanedValue);
+  };
+
+  const handlePasswordChange = (event) => {
+    const cleanedValue = event.target.value.replace(/\s/g, '');
+    setPassword(cleanedValue);
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="App">
+      <h1>WayToGo</h1>
+      <h2>Username</h2>
+      <input
+        type="text"
+        className="input-box"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <h2>Password</h2>
+      <div style={{ position: 'relative' }}>
+        <input
+          type={showPassword ? 'text' : 'password'}
+          className="input-box"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button
+          type="button"
+          onClick={handleTogglePassword}
+          style={{
+            top: '50%',
+            transform: 'translateY(-50%)',
+            border: 'none',
+            background: 'transparent',
+            cursor: 'pointer',
+          }}
+        >
+          {showPassword ? 'Hide' : 'Show'}
+        </button>
+      </div>
+      <button>Sign In</button>
+    </div>
+  );
 }
+
+export default App;
