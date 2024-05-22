33077be83d7f74ce5f0fbf2db38622b629c4c699
molly-culot <molly.culot@icloud.com>
2024-05-22 15:39:54 -0400

    bais

---
+++ react-website/src/App.js
@@ -0,0 +1,35 @@
+
+// Filename - App.js
+ 
+import React from "react";
+import Navbar from "./components/Navbar";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./pages";
+import LogSchedule from "./pages/LogSchedule";
+import SignUp from "./pages/SignUp";
+import ViewSchedule from "./pages/ViewSchedule";
+ 
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route
+                    path="/contact"
+                    element={<Contact />}
+                />
+                <Route path="/blogs" element={<Blogs />} />
+                <Route
+                    path="/sign-up"
+                    element={<SignUp />}
+                />
+            </Routes>
+        </Router>
+    );
+}
